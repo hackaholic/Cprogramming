183d4ebadcc748a699885e2361e38cec6b67cce3
kumars <kumars@parallelwireless.com>
2021-07-16 18:29:16 +0530

    Added code for varadic function variable length argument

---
+++ basic/va_argument.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum(int i, ...) {
+    va_list ap;
+    int total = 0;
+
+    va_start(ap, i); // initilization ap to point 1st arg
+    total += i;
+
+    while (i) {
+        i = va_arg(ap, int);
+        total += i;
+    }
+
+    return total;
+}
+
+int main() {
+    printf("sum(1,2,3,4,5,6)\n");
+    printf("%i\n", sum(1,2,3,4,5,6));
+}
