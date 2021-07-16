2cbe29d7cd599af5f75766c77fb756c2127b8ee4
kumars <kumars@parallelwireless.com>
2021-07-16 21:14:40 +0530

    Added variable argument sumFloat function

--- basic/va_argument.c
+++ basic/va_argument.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int sum(int i, ...) {
+int sumInt(int i, ...) {
     va_list ap;
     int total = 0;
 
@@ -16,7 +16,30 @@ int sum(int i, ...) {
     return total;
 }
 
+
+
+/*
+char, sort int will be promoted to  int, float will be promoted to double.
+Compalier warning when float is passed
+va_argument.c:26:24: warning: ‘float’ is promoted to ‘double’ when passed through ‘...’
+   26 |         i = va_arg(ap, float);
+      |                        ^
+*/
+float sumFloat(double i, ...) {
+    va_list ap;
+    double total = 0.0;
+    va_start(ap, i);
+    total += i;
+    while (i) {
+        i = va_arg(ap, double);
+        printf("%f\n", i);
+        total += i;
+    }
+    return total;
+}
+
 int main() {
-    printf("sum(1,2,3,4,5,6)\n");
-    printf("%i\n", sum(1,2,3,4,5,6));
+    printf("sumInt(1, 2, 3, 4, 5, 6) -> %i\n", sumInt(1, 2, 3, 4, 5, 6));
+
+    printf("sumFloat(1.2, 3.4, 6.6, 9.9) -> %f\n", sumFloat(1.2, 3.4, 6.6, 9.9));
 }
