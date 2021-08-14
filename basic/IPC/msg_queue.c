2348898085bf3650a29a090b1ff057aaa1caa53d
kumars <kumars@parallelwireless.com>
2021-08-14 16:09:37 +0530

    Practice IPC, msgctl, msqid_ds

--- basic/IPC/msg_queue.c
+++ basic/IPC/msg_queue.c
@@ -11,6 +11,7 @@
 #include <sys/types.h>
 #include <sys/msg.h>
 #include <sys/ipc.h>
+#include <time.h>
 
 #define MAX_PATHNAME_SIZE 100
 #define MAX_MESSAGE_SIZE 100
@@ -21,8 +22,22 @@ static struct mymsg {
         char text[MAX_MESSAGE_SIZE];
     } msg;
 
+void get_queue_metadata(int id) {
+    struct msqid_ds queue_meta;
+    // int msgctl(int msqid, int cmd, struct msqid_ds *buf);
+    msgctl(id, IPC_STAT, &queue_meta);
+    printf("Message queue: %i\n", id);
+    printf("msg_qnum: %lu\n", queue_meta.msg_qnum);
+    printf("Total bytes in queue: %lu\n", queue_meta.__msg_cbytes);
+    printf("Maximum BYTES allowed in queue %lu\n", queue_meta.msg_qbytes);
+    printf("Last msg send Time: %s", asctime(gmtime(&queue_meta.msg_stime)));
+    printf("Last msg rcv Time: %s", asctime(gmtime(&queue_meta.msg_rtime)));
+    printf("Last change Time: %s", asctime(gmtime(&queue_meta.msg_ctime)));
+}
+
 int open_msg_queue(key_t key) {
     int id;
+    // int msgget(key_t key, int msgflg);
     if ((id = msgget(key, IPC_CREAT | 0660)) == -1) {
         printf("Error while creating msg Queue\n");
         return -1;
@@ -34,20 +49,25 @@ int msg_snd(int id, struct mymsg *buf) {
     int status;
     size_t msg_size;
     msg_size = sizeof(struct mymsg);
+    // int msgsnd(int msqid, const void *msgp, size_t msgsz, int msgflg);
     if((status = msgsnd(id, buf, msg_size, 0)) == -1) {
         printf("Error in appending msg to queue %i\n, %d\n", id, EXIT_FAILURE);
         return status;
     }
+    get_queue_metadata(id);
     return status;
 }
 
-size_t msg_rcv(int id, struct mymsg *buf) {
-    size_t msg_size;
+ssize_t msg_rcv(int id, struct mymsg *buf) {
+    ssize_t msg_size;
     int length;
     length = sizeof(struct mymsg);
+    // ssize_t msgrcv(int msqid, void *msgp, size_t msgsz, long msgtyp, int msgflg);
     if ((msg_size = msgrcv(id, buf, length, 0, 0)) == -1) {
         printf("Error in fetching the msg from the queue: %i\n", id);
+        return msg_size;
     }
+    get_queue_metadata(id);
     return msg_size;
 }
 
@@ -68,7 +88,7 @@ int main(int argc, char *argv[]) {
         printf("File path too long\n");
         exit(1);
     }
-    strcpy(filepath, *++argv);
+    strcpy(filepath, argv[1]);
     printf("filepath: %s\n", filepath);
     
     // Create IPC identier key
@@ -83,11 +103,12 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
     printf("msg_queue_id: %i\n", msg_queue_id);
-    strcpy(mode, *++argv);
+    strcpy(mode, argv[2]);
     if (strcmp(mode, "s") == 0) {
         msg.msg_type = 1;
         strcpy(msg.text, "Hello World");
         status = msg_snd(msg_queue_id, &msg);
+        printf("Msg sent...\n");
     }
     if (strcmp(mode, "r") == 0) {
         msg_size = msg_rcv(msg_queue_id, &msg);
