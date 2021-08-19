0b42ff6a871a540127fee593cb5cb135c3b2396c
kumars <kumars@parallelwireless.com>
2021-08-19 23:29:30 +0530

    OS: process fork, execvp code practice

---
+++ os_three_easy_pieces/5/exec_test.c
@@ -0,0 +1,46 @@
+/* 
+    Author: Kumar Shubham
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+#define MAX_ARGS 5
+
+int main(int argc, char *argv[]) {
+    pid_t childpid;
+    int i;
+    char *myargs[MAX_ARGS];
+    if(argc > MAX_ARGS) {
+        printf("Max Arg allowed %i\n", MAX_ARGS);
+        exit(EXIT_FAILURE);
+    }
+    if (( childpid = fork()) == -1) {
+        printf("Error in creating child process\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(childpid == 0) {
+        printf("I am Child Process: %i\n", getpid());
+        for(i=1;i<argc && argc<=MAX_ARGS;i++) {
+            myargs[i-1] = argv[i];
+        }
+        myargs[i-1] == NULL;
+        printf("Command to run: ");
+        for(i=0; i<argc-1;i++){
+            printf("%s ", myargs[i]);
+        }
+        printf("\n");
+        execvp(myargs[0], myargs);
+        printf("I will not get printed\n");
+    }
+    if(childpid > 0) {
+        printf("I am parent Process: %i\n", getpid());
+        //try with and without wait system call
+       pid_t child_pid = wait(NULL);
+    }
+}
