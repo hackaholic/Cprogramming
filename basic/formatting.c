aeaf3a780e5728be6c74608a873a977bd251a9de
kumars <kumars@parallelwireless.com>
2023-01-18 06:14:56 +0000

    basic: scanf tips and tricks

--- basic/formatting.c
+++ basic/formatting.c
@@ -7,9 +7,9 @@ int main() {
 
     // formatting practice, printf returns number of character printed
     printf(":%s:\n", "Hello World");
-    printf(":%30s:\n", "Hello World");
-    printf(":%-30s:\n", "Hello World"); // Left alignment
-    printf(":%.5s:\n", "Hello World");
+    printf(":%30s:\n", "Hello World");   // right alignment
+    printf(":%-30s:\n", "Hello World");  // Left alignment
+    printf(":%.5s:\n", "Hello World");   // only take 5 char
     printf(":%20.5s:\n", "Hello World");
 
     char arr[20];
@@ -33,4 +33,43 @@ int main() {
     char a[10], b[10], c[10];
     n = sscanf(arr, "%s %s %s", a, b, c);
     printf("%s %s %s\n", a, b ,c);
+
+
+    // more on scanf 
+    
+    int d;
+    char str[50];
+    /*
+        for limit use width like %5d, it will take only 5 digit input
+
+        so input 123456789 it will only read 12345
+        for string input hellotherecoco, it will read hellothere
+    */
+    scanf("%5d", &d);
+    printf("%d\n", d);
+    
+    while(getchar() != 10);       // this is to flush remaining digit on stdin before moving to next
+
+    scanf("%10s", str);
+    printf("%s\n", str);
+
+    /*
+        selective selection using []
+        %[a-z] will only select letter contain a-z
+        '^' is used for up to
+        %[^\n] will pickup all upto \n
+
+        * is used to exclude
+        %*c, it will execude a char
+        
+        %*[^,]  it will execude all upto , 
+
+        %n record the scanf offset
+    */ 
+
+    sscanf("(abcd, 10)", "(%50[^,], %i %n", str, &d, &n);
+    printf("%s; %i, offset: %i\n", str, d, n);
+
+    return 0;
+
 }
