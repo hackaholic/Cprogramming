fb39a5b806ac30d175a23ee76235fe1ca44c3f45
kumars <kumars@parallelwireless.com>
2023-02-25 10:33:48 +0000

    intro_to_algo: Dynamic prograaming , find subset equal to sum and print all combinations

--- intro_to_algo/15_Dynamic_Programming/subset_sum.c
+++ intro_to_algo/15_Dynamic_Programming/subset_sum.c
@@ -28,7 +28,58 @@ int findsubset(int *a, int n, int sum) {
     return findsubset(a, n-1, sum-a[n-1]) || findsubset(a, n-1, sum); 
 }
 
+int dp_findsubset(int *a, int n, int sum){
+    int i,j,x,y;
 
+    int t[sum+1][n];
+    for(i=0;i<sum;i++) {
+        t[0][i] = 1;
+    }
+
+    for(i=1;i<=sum;i++) {
+        for(j=0;j<n;j++){
+            // including the element;
+            x = (i-a[j]>=0)?t[i-a[j]][j]:0;
+
+            // excluding the element
+            y = (j>=1)?t[i][j-1]:0;
+
+            t[i][j] = x || y;
+        }
+    }
+
+    return t[sum][n-1];
+
+}
+
+
+void get_all_combinations(int *a, int n, int sum, int *path, int index) {
+    int i;
+    if(sum == 0){
+        for(i=0;i<index;i++){
+            printf("%i ", path[i]);
+        }
+        printf("\n");
+        return;
+    }
+
+    if(sum<0 || n==0){
+        return;
+    }
+
+    if(a[0] > sum) {
+        get_all_combinations(a, n-1, sum, path, index);
+        return;
+    }
+
+    // include the element
+    path[index] = a[n-1];
+    get_all_combinations(a, n-1, sum-a[n-1], path, index+1);
+
+    //exclude the element
+    get_all_combinations(a, n-1, sum, path, index);
+
+}
 
 int main(int argc, char *argv[]) {
 
@@ -41,6 +92,15 @@ int main(int argc, char *argv[]) {
     else {
         printf("False\n");
     }
+
+    if(dp_findsubset(arr, len, sum)){
+        printf("True\n");
+    }
+    else {
+        printf("False\n");
+    }
     
+    int path[len];
+    get_all_combinations(arr, len, sum, path, 0);
     return 0;
 }
